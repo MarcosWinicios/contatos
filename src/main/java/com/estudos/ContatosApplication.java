c2a45d6be816e6f517c8432b22ecec0b8b3ab573
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2023-12-04 01:22:05 -0300

    Create Project

---
+++ src/main/java/com/estudos/ContatosApplication.java
@@ -0,0 +1,14 @@
+package com.estudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatosApplication {
+
+	public static void main(String[] args) {
+		System.out.println("teste");
+		SpringApplication.run(ContatosApplication.class, args);
+	}
+
+}
